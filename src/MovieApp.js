098b36f573577a4eefc107773b7d906069216ae5
ronaldoisc <tellezleal9928@gmail.com>
2022-01-19 20:46:03 -0600

    change the background color

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -5,25 +5,38 @@ import { useFetchMovie } from './hooks/useFetchMovie';
 
 export const MovieApp = () => {
 
-    const {data:movies,loanding} = useFetchMovie();
+    const { data: movies, loanding } = useFetchMovie();
 
-    
+
     return (
         <div>
-            <HeaderMovie/> 
-            
-            <h3>Películas Recientes...</h3>
-            <div className="cards">
-            {loanding && <p>Cargando</p>}
-            
             {
-                movies.map(movie => {
-                      return <CardMovie key={movie.id} movie={movie}/>
-                })
+                loanding ?
+                   <div className='d-flex flex-column align-items-center justify-content-center vh-100'>
+                       <h1 className='font-weight-bold'>WELCOME</h1>
+                       <h1 className='font-weight-normal'>MOVIE APP</h1>
+
+                   </div>
+                    :
+                    <div>
+                        <HeaderMovie />
+
+                        <h1 className='text-center'>New Movies</h1>
+                        <div className="cards">
+
+
+                            {
+                                movies.map(movie => {
+                                    return <CardMovie key={movie.id} movie={movie} />
+                                })
+
+                            }
+
+                        </div>
+                    </div>
 
             }
-                
-            </div>
+
 
         </div>
     )
